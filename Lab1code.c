31d4e92029653fad446403ff7453189c8aa6f154
Dmitry <dimon.gritsenko8699@yandex.ru>
2017-09-26 23:34:13 +0300

    Lab1
    Лабораторная работа №1, исходный код и исполняемый файл.

---
+++ Lab1code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	setlocale(0, "Russian");
+	float a,b,c;
+	int rez = 0;
+
+	printf("Enter the distance in decimeters   ");
+	do {
+		rez = scanf("%f", &a);
+		if (!rez) {
+			printf("Error. Incorrect data entered. Try again: ");
+			while (getchar() != '\n');
+		}
+	} while (rez != 1);
+    
+	b=a / 1.852;
+	 printf("\n It %f palms(ladoney)",b);
+	c=a / 10668;
+ 	 printf(" and %f verst \n",c);
+	
+	system("pause");
+	
+	return 0;
+}
