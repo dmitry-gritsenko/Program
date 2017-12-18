6e2e818ce482ed8eb7ce266ce52a003238c0cedf
Dmitry <31659108+dmitry-gritsenko@users.noreply.github.com>
2017-12-18 22:25:34 +0300

    Исправил код согласно issues

--- Lab4code.c
+++ Lab4code.c
@@ -10,44 +10,35 @@
 #include <stdio.h>  
 #include <string.h> 
 #include <conio.h>
-#include <process.h>
-#include <iostream>
 
 #define NUM 1024
 
-/*Функция замены строки
-строка
-что заменяем
-на что заменяем
-буффер
-Replace(str, a, b, buffer);*/
-
 void Replace(char str[], char a[], char b[], char buffer[])
 	{
-		int i, j, pos = 0;
-		for (i = 0; str[i]; i++)
+		int pos = 0;
+		for (int i = 0; str[i]; i++)
 		{	
-			for (j = 0; str[i + j] && a[j]; j++)   //начинается ли с i-ой позиции строка a
-				if (str[i + j] != a[j]) break;
-				if (!a[j])  //если a[j]=0, то мы дошли до конца строки a
-			{
-			i += j - 1;  //в j у нас длина стоки a, которую мы перепрыгиваем
-			for (j = 0; b[j]; j++) buffer[pos++] = b[j];  //на место стоки a ставим строку b
-			}
-		else
-		{
+			for (int j = 0; str[i + j] && a[j]; j++)   //начинается ли с i-ой позиции строка a
+				if (str[i + j] != a[j]) 
+					break;
+			if (!a[j])  //если a[j]=0, то мы дошли до конца строки a
+				{
+				i += j - 1;  //в j у нас длина стоки a, которую мы перепрыгиваем
+				for (j = 0; b[j]; j++) buffer[pos++] = b[j];  //на место стоки a ставим строку b
+				}
+			else
+				{
 			buffer[pos++] = str[i];   //просто копируем символ
+				}
+			buffer[pos] = NULL;  //ставим конец строки
 		}
-		buffer[pos] = NULL;  //ставим конец строки
-		}
-	strcpy(str, buffer);  //копируем в первоначальную строку
+		strcpy(str, buffer);  //копируем в первоначальную строку
 	}
 int main(void)
 	{
-	//setlocale(0, ""); // включаем кириллицу в консоли
 	char * OutFileName = "Lab4_output.txt";
 	FILE * InputFileName = fopen("Lab4_input.txt", "r");
-	bool result = 0;
+	int result = 0;
 	// Массив со строкой для поиска
 	char str[NUM];
 	//scanf("%s", &str);
@@ -69,17 +60,13 @@ int main(void)
 	// Выделение последующих частей
 	while (istr != NULL)
 	{
-		// Вывод очередной выделенной части
-		if ((strlen(istr) % 2) == 1) {
-			//printf("%s\n", istr);
-			//bool result = fputs(istr'\n', file);
+		if ((strlen(istr) % 2) == 1) 
+			{
 			result = fprintf(file, "%s\n", istr);
-		}
+			}
 		// Выделение очередной части строки
 		istr = strtok(NULL, sep);
 	}
-	//if (!result) // если запись произошла успешно
-	//printf("Строка в файл успешно записана!\n");// выводим сообщение
 	if (!file) 
 	printf("Нет доступа к файлу!\n");
  
